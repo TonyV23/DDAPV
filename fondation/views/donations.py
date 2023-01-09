bb13821fdeb306ee4e6bd469b3bd6ba8c20716e3
viper23 <htonnycarlos@gmail.com>
2023-01-09 12:57:50 +0200

    add donations controller

---
+++ fondation/views/donations.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+
+
+def index(request) :
+
+    page_title = 'Aperçu sur les aides et dons'
+
+
+    return render(
+        request,
+        'fondation/donations/index.html',
+        {
+            'page_title' : page_title
+        }
+    )
+
+def add_type(request) :
+
+    page_title = 'Nouveau type de dons ou aides'
+
+    return render(
+        request,
+        'fondation/donations/donations_type.html',
+        {
+            'page_title' : page_title
+        }
+    )
+
+
+def add_donation_help(request) :
+
+    page_title = 'Nouveau aide ou don'
+
+    return render(
+        request,
+        'fondation/donations/donations_help.html',
+        {
+            'page_title' : page_title
+        }
+    )
+
+def display(request) :
+    
+    page_title = 'Liste des dons et aides'
+
+    return render(
+        request,
+        'fondation/donations/display.html',
+        {
+            'page_title' : page_title
+        }
+    )
