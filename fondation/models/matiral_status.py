3f13face8dd2f40b87c0c2e74f4f93ff7112d845
viper23 <htonnycarlos@gmail.com>
2023-01-11 11:24:01 +0200

    changed attribute name

--- fondation/models/matiral_status.py
+++ fondation/models/matiral_status.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class MatiralStatus(models.Model) :
     
-    name = models.CharField('situation matrimoniale', max_length=20, help_text='tapez la situation matrimoniale', unique= True)
+    matiral_status_name = models.CharField('situation matrimoniale', max_length=20, help_text='tapez la situation matrimoniale', unique= True)
 
     def __str__(self) -> str:
-        return self.name
+        return self.matiral_status_name
