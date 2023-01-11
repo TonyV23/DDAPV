75a8a244d5b4848d29db437dbb26394ca219d5e1
viper23 <htonnycarlos@gmail.com>
2023-01-11 11:23:45 +0200

    changed attribute name

--- fondation/models/level_studies.py
+++ fondation/models/level_studies.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class LevelStudy(models.Model) : 
     
-    name = models.CharField("Niveau d'études", max_length=30, help_text = "tapez le niveau d'études", unique=True)
+    level_study_name = models.CharField("Niveau d'études", max_length=30, help_text = "tapez le niveau d'études", unique=True)
 
     def __str__(self) -> str:
-        return self.name
+        return self.level_study_name
