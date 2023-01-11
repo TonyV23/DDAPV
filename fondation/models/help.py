63a53db881ca741aeb411d2754e21ef0016d2ec7
viper23 <htonnycarlos@gmail.com>
2023-01-11 11:23:19 +0200

    changed attribute name class and remove ordering property

--- fondation/models/help.py
+++ fondation/models/help.py
@@ -4,18 +4,16 @@ from fondation.models import TypeHelp
 
 class Help (models.Model) : 
     
-    type_help = models.ForeignKey(TypeHelp, on_delete=models.CASCADE)
-    name = models.CharField("aides", max_length=40, unique=True, help_text='ex : vivre alimentaires , personnes vivant avec handicap')
+    type_help_name = models.ForeignKey(TypeHelp, on_delete=models.CASCADE)
+    help_name = models.CharField("aides", max_length=40, unique=True, help_text='ex : vivre alimentaires , personnes vivant avec handicap')
 
     def __str__(self) -> str:
-        return self.name
+        return self.help_name+', '+self.type_help_name
 
     class Meta :
         constraints = [
             models.UniqueConstraint(
-                fields = ['type_help', 'name'],
+                fields = ['type_help_name', 'help_name'],
                 name = 'unique_aide'
             )
         ]
-        
-        ordering = ['type_help', 'name']
