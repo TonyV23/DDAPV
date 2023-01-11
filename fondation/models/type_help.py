e6fa9f6a760606f588562c56b54ffba3821c49dc
viper23 <htonnycarlos@gmail.com>
2023-01-11 11:24:43 +0200

    modified attribute name class

--- fondation/models/type_help.py
+++ fondation/models/type_help.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class TypeHelp(models.Model) :
 
-    name = models.CharField("type d'aides", max_length=40, unique=True, help_text='ex : aides en nature , aides en espece')
+    type_help_name = models.CharField("type d'aide", max_length=40, unique=True, help_text='ex : aides en nature , aides en espece')
 
     def __str__(self) -> str:
-        return self.name
+        return self.type_help_name
