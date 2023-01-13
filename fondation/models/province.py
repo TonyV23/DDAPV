4549ddc05298f37d9c510d62c5738a69e01c0755
viper23 <htonnycarlos@gmail.com>
2023-01-13 09:46:44 +0200

    - Change Meta options on province     - Remove field province_name from province     - Add field nom_province to province

--- fondation/models/province.py
+++ fondation/models/province.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Province(models.Model) :
     
-    province_name = models.CharField('Nom du province', max_length=20, help_text='taper le nom du province', unique = True)
+    nom_province = models.CharField(max_length=20, help_text='taper le nom du province', unique = True)
 
     def __str__(self) -> str:
-        return self.province_name
+        return self.nom_province
          
     
     class Meta : 
-        ordering = ['province_name']
+        ordering = ['nom_province']
