2b8c51e3cd2e5057876f5d88c5abb81a2c6f0c37
viper23 <htonnycarlos@gmail.com>
2023-01-13 11:29:12 +0200

    removed help and type_help model

--- fondation/models/type_help.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class TypeHelp(models.Model) :
-
-    type_help_name = models.CharField("type d'aide", max_length=40, unique=True, help_text='ex : aides en nature , aides en espece')
-
-    def __str__(self) -> str:
-        return self.type_help_name
