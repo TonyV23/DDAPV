ad86524aaed207d476f959386bde59299f18e9d7
viper23 <htonnycarlos@gmail.com>
2023-01-14 18:57:34 +0200

    add from .vulnerability_form import VulnerabilityForm

--- fondation/forms/__init__.py
+++ fondation/forms/__init__.py
@@ -2,3 +2,4 @@ from .province_form import ProvinceForm
 from .commune_form import CommuneForm
 from .level_study_form import LevelStudyForm
 from .matiral_status_form import MatiralStatusForm
+from .vulnerability_form import VulnerabilityForm
