e250ccc61a0977b56ecc7cbbca9a5be9090e7d0b
viper23 <htonnycarlos@gmail.com>
2023-01-19 18:47:25 +0200

    and count total number of refugees, camps, donors and distributions

--- fondation/views/home.py
+++ fondation/views/home.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
 
 
+
+from fondation.models import Person, Donor, Camp, Distribution
+
+
 def index(request) :
 
     page_title = 'Tableau de Bord'
+    template = 'fondation/home/index.html'
 
+    total_refugees = Person.objects.count()
+    total_camps = Camp.objects.count()
+    total_donors = Donor.objects.count()
+    total_distributions = Distribution.objects.count()
+
+
+
+
+
+    variable = {
+
+        'page_title' : page_title,
+        
+        'total_refugees' : total_refugees,
+        'total_camps' : total_camps,
+        'total_donors' : total_donors,
+        'total_distributions' : total_distributions,
+    
+    }
 
     return render(
         request,
-        'fondation/home/index.html',
-        {
-            'page_title' : page_title
-        }
+        template_name = template,
+        context = variable
     )
