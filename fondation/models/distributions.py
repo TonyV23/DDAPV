895f3f7e9d7884a6356486e95629602d63b31254
viper23 <htonnycarlos@gmail.com>
2023-01-27 01:49:46 +0200

    modified distribution model

--- fondation/models/distributions.py
+++ fondation/models/distributions.py
@@ -1,12 +1,12 @@
 from django.db import models
-from multiselectfield import MultiSelectField
 
-
-from fondation.models import Camp, Person
+from fondation.models import Province, Commune, Person
 
 class Distribution(models.Model) : 
     
-    camp = models.ForeignKey(Camp, on_delete=models.CASCADE, help_text="sélectionez le camp du refugié (e)")
+    province = models.ForeignKey(Province, on_delete=models.CASCADE)
+    commune =  models.ForeignKey(Commune, on_delete=models.CASCADE)
+    zone =  models.CharField(max_length=50)
     beneficiaire = models.ForeignKey(Person, on_delete=models.CASCADE ,help_text="sélectionez le (la) bénéficiaire", unique_for_date='date_given')
     
     TypeHelpChoices = (
@@ -15,7 +15,7 @@ class Distribution(models.Model) :
         ('accompagnement_psychosocial','Accompagnement psychosocial'),
         ('accompagnement_medical','Accompagnement médical')
     )    
-    type_aide = MultiSelectField(choices=TypeHelpChoices, max_length=150)
+    type_aide = models.CharField(choices=TypeHelpChoices, max_length=150)
 
     TypeAssistChoices = (
         ('vivre_alimentaire','Vivre alimentaire'),
@@ -32,11 +32,11 @@ class Distribution(models.Model) :
         ('reinstallation','Réinstallation'),
         ('kinesitherapie','kinésithérapie'),
     )
-    type_assistance = MultiSelectField(choices=TypeAssistChoices, max_length=150)
+    type_assistance = models.CharField(choices=TypeAssistChoices, max_length=500)
 
-    description = models.CharField("Autres",max_length=500, help_text='reseignez autre type d\'assistance qui ne figure pas dans la liste', blank=True)
+    description = models.TextField(max_length=500, help_text='reseignez autre type d\'assistance qui ne figure pas dans la liste', blank=True)
     
     date_given = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self) -> str:
-        return self.camp+"  "+self.beneficiaire+"  "+self.type_assistance+"  "+self.description
+    def __str__(self)-> str :
+        return self.province+" - "+self.commune+" - "+self.beneficiaire+" - "+self.type_assistance+" - "+self.description
