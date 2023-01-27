bc49fea7da1c6d4a8f7a5a297b02b5e5d4d6219b
viper23 <htonnycarlos@gmail.com>
2023-01-27 03:04:11 +0200

    modified person model

--- fondation/models/person.py
+++ fondation/models/person.py
@@ -1,7 +1,7 @@
 from django.db import models
 from phone_field import PhoneField
 
-from fondation.models import Camp, Province, Commune, Vulnerability, LevelStudy, MatiralStatus
+from fondation.models import Province, Commune, Vulnerability, LevelStudy, MatiralStatus
 
 class Person (models.Model) :
     
@@ -16,7 +16,6 @@ class Person (models.Model) :
     nom_prenom_du_pere = models.CharField(max_length=50,help_text='tapez le nom et le prenom du père')
     nom_prenom_de_la_mere = models.CharField(max_length=50,help_text='tapez le nom et le prenom du mère')
 
-    camp = models.ForeignKey(Camp, on_delete=models.CASCADE, help_text="sélectionez le camp du refugié")
     nom_de_la_province = models.ForeignKey(Province, on_delete=models.CASCADE, help_text="province d'origine")
     nom_de_la_commune = models.ForeignKey(Commune, on_delete=models.CASCADE, help_text="commune d'origine")
     nom_de_la_zone = models.CharField('zone', max_length=20, help_text="zone d'origine")
@@ -26,18 +25,18 @@ class Person (models.Model) :
     situation_matrimoniale = models.ForeignKey(MatiralStatus, on_delete=models.CASCADE, help_text="sélectionez sa situation de situation matrimoniale")
 
     nombre_enfants =  models.PositiveIntegerField(help_text="renseignez le nombre d'enfants à sa charge")
-    fonction_avant_exil = models.CharField(max_length=50, blank=True)
+    fonction = models.CharField(max_length=50, blank=True)
 
     date_joined = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) :
-        return self.nom+' '+self.prenom+' '+self.sexe
+        return self.nom+'--'+self.prenom+'--'+self.sexe
 
     class Meta : 
 
         constraints = [
             models.UniqueConstraint(
-            fields = ['nom','prenom','sexe','age','numero_de_telephone','nom_prenom_du_pere','nom_prenom_de_la_mere','camp','nom_de_la_province','nom_de_la_commune','nom_de_la_zone','la_vulnerabilite','le_niveau_etudes','situation_matrimoniale','nombre_enfants','fonction_avant_exil', 'date_joined'],
+            fields = ['nom','prenom','sexe','age','numero_de_telephone','nom_prenom_du_pere','nom_prenom_de_la_mere','nom_de_la_province','nom_de_la_commune','nom_de_la_zone','la_vulnerabilite','le_niveau_etudes','situation_matrimoniale','nombre_enfants','fonction', 'date_joined'],
             
             name = 'unique_person'
             )
