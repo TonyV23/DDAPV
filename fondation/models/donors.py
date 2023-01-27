adb26e5a616a02b77e248fa566f3a4317b23db70
viper23 <htonnycarlos@gmail.com>
2023-01-27 02:15:47 +0200

    modified donor model

--- fondation/models/donors.py
+++ fondation/models/donors.py
@@ -1,13 +1,9 @@
 from django.db import models
 
-from multiselectfield import MultiSelectField
 from phone_field import PhoneField
-
-from fondation.models import Camp
 
 class Donor(models.Model) : 
 
-    camp = models.ForeignKey(Camp, on_delete=models.CASCADE, help_text="sélectionez le camp")     
     TypeDonor = (
         ('org', 'Organisation'),
         ('entrp', 'Entreprise'),
@@ -16,6 +12,7 @@ class Donor(models.Model) :
         ('anonym','Rester anonyme')
     )
     type_donneur = models.CharField(choices=TypeDonor, max_length=50)
+
     nom_du_donneur = models.CharField(max_length=50, help_text="tapez votre nom / le nom de l'entreprise , de l'association ou de l'organisation", blank=True)
 
     TypeHelpChoices = (
@@ -24,7 +21,7 @@ class Donor(models.Model) :
         ('accompagnement_psychosocial','Accompagnement psychosocial'),
         ('accompagnement_medical','Accompagnement médical')
     )
-    type_aide = MultiSelectField(choices=TypeHelpChoices, max_length=150)
+    type_aide = models.CharField(choices=TypeHelpChoices, max_length=150)
 
     TypeAssistChoices = (
         ('vivre_alimentaire','Vivre alimentaire'),
@@ -42,15 +39,12 @@ class Donor(models.Model) :
         ('kinesitherapie','kinésithérapie'),
     )
 
-    type_assistance = MultiSelectField(choices=TypeAssistChoices, max_length=150)
-
-    description = models.CharField("Autres",max_length=500, help_text='décrivez le type de don et/ou aides que vous souhaitez donner à notre fondation',blank=True)
+    type_assistance = models.CharField(choices=TypeAssistChoices, max_length=150)
 
     numero_de_telephone = PhoneField(help_text='ce numero de téléphone sera utilisé pour entrer en contact avec vous')
+
     adresse_mail = models.EmailField(help_text='cette addresse mail sera utilisée pour entrer en contact avec vous')
 
-    donation_received = models.BooleanField(default=0)
-    donor_date_given = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=500, help_text='décrivez le type de don et/ou aides que vous souhaitez donner à notre fondation',blank=True)
 
-    def __str__(self) -> str:
-        return self.camp+' '+self.type_donneur+' '+self.nom_du_donneur+' '+self.type_aide+' '+self.description+''+self.numero_de_telephone+' '+self.adresse_mail+' '+self.donor_date_given
+    donor_date_given = models.DateTimeField(auto_now_add=True)
