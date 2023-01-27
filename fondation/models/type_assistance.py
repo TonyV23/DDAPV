024842ccb29a6dc8ead80e31aa2d05e84ed19daa
viper23 <htonnycarlos@gmail.com>
2023-01-27 08:29:04 +0200

    add typeAssistance models

---
+++ fondation/models/type_assistance.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from fondation.models import TypeAide
+
+class TypeAssistance(models.Model) :
+    
+    type_aide = models.ForeignKey(TypeAide, on_delete=models.CASCADE)
+    type_assistance = models.CharField(max_length=20, unique = True)
+
+    def __str__(self) -> str:
+        return self.type_assistance
+    
+    class Meta :
+        constraints = [
+            models.UniqueConstraint(
+                fields = ['type_aide','type_assistance'],
+                name = 'unique_type_assistance'
+            )
+        ]
