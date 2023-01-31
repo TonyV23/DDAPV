e19111312470e2cfc34181730e661229372d847d
viper23 <htonnycarlos@gmail.com>
2023-02-01 00:59:54 +0200

    add from .refugees_employee import * from .donations_employee import * from .distributions_employee import * from .distributions import *

--- fondation/views/__init__.py
+++ fondation/views/__init__.py
@@ -3,3 +3,7 @@ from .refugees import *
 from .donations import *
 from .address import *
 from .level_studies import *
+from .refugees_employee import *
+from .donations_employee import *
+from .distributions_employee import *
+from .distributions import *
