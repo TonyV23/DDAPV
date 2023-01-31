3ec41e906d658c79f110516e4ee8608dbc47d134
viper23 <htonnycarlos@gmail.com>
2023-01-31 23:07:03 +0200

    add allowed user decorator

---
+++ fondation/decorators/allowed_users.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+def allowed_users(allowed_roles = []) :
+    def decorator(view_func) :
+        def wrapper_func(request, *args, **kwargs) :
+            group = None
+            if request.user.groups.exists() :
+                group = request.user.groups.all()[0].name
+                if group in allowed_roles :
+                    return view_func(request, *args, **kwargs)
+                else :
+                    return HttpResponse('Vous n\'êtes pas autorisés à acceder à cette page')
+        return wrapper_func
+    return decorator
