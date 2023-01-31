6a48973167928d094dcc545aee98234aa397a057
viper23 <htonnycarlos@gmail.com>
2023-01-31 10:52:59 +0200

    add userOverview, userList, userRegister and userStore view

--- fondation/views/auth.py
+++ fondation/views/auth.py
@@ -4,6 +4,62 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+from fondation.forms import UserForm
+
+def index(request):
+    page_title = 'Aperçu sur les utilisateurs'
+    template = 'fondation/user/index.html'
+
+    variable = {
+        'page_title' : page_title,
+    }
+
+    return render(
+        request,
+        template_name = template,
+        context = variable
+    )
+
+def userList(request) :
+    page_title = 'Liste des utilisateurs'
+    users = User.objects.all()
+    template = 'fondation/user/list.html'
+
+    variable = {
+        'page_title' : page_title,
+        'users' : users
+    }
+
+    return render(
+        request,
+        template_name = template,
+        context = variable
+    )
+
+def userRegister(request) :
+    page_title = 'Nouveau utilisateur'
+    template = 'fondation/user/register.html'
+    form = UserForm()
+
+    variable = {
+        'page_title' : page_title,
+        'form': form
+    }
+    
+    return render(
+        request, 
+        template_name = template,
+        context = variable
+    )
+
+def userStore(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Le compte de '+user+' a été ajouté')
+        return redirect('/userList/')
 
 def userLogin(request) :
     if request.method == 'POST':
@@ -34,5 +90,5 @@ def userLogin(request) :
 
 def userLogout(request) :
     logout(request)
-    
+
     return redirect('/login')
