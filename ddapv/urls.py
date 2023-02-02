a15e981df9a57425feaaf9810d30df67ec71638a
viper23 <htonnycarlos@gmail.com>
2023-02-02 18:33:46 +0200

    add django.contrib.auth.urls

--- ddapv/urls.py
+++ ddapv/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('fondation.urls'))
+    path('', include('fondation.urls')),
+    path ('', include('django.contrib.auth.urls')),
 ]
