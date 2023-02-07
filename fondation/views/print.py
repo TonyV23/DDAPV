b7b78edd540f6c14c316069271dba4a1f6bfe7de
viper23 <htonnycarlos@gmail.com>
2023-02-07 13:01:00 +0200

    add print person view

--- fondation/views/print.py
+++ fondation/views/print.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from fondation.models import Donor, Distribution
+from fondation.models import Donor, Distribution, Person
 
 @login_required(login_url ='login')
 def printDonors(request) :
@@ -35,6 +35,26 @@ def printDistributions(request) :
     variable  = {
             'page_title' : page_title,
             'distributions' : distributions
+        }
+
+    return render(
+        request,
+        template_name = template,
+        context = variable 
+    )
+
+@login_required(login_url ='login')
+def printPersons(request) :
+
+    page_title = 'Impression des vulnérables'
+    template = 'fondation/refugees/print.html'
+
+    refugees = Person.objects.all()
+
+
+    variable  = {
+            'page_title' : page_title,
+            'refugees' : refugees
         }
 
     return render(
