ecefa72c611f3991e7a0487bca3a5a879e9821d1
viper23 <htonnycarlos@gmail.com>
2023-02-07 12:42:51 +0200

    printDistributions view

--- fondation/views/print.py
+++ fondation/views/print.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from fondation.models import Donor
+from fondation.models import Donor, Distribution
 
 @login_required(login_url ='login')
 def printDonors(request) :
@@ -15,6 +15,26 @@ def printDonors(request) :
     variable  = {
             'page_title' : page_title,
             'donors' : donors
+        }
+
+    return render(
+        request,
+        template_name = template,
+        context = variable 
+    )
+
+@login_required(login_url ='login')
+def printDistributions(request) :
+
+    page_title = 'Impression des bénéficiaires'
+    template = 'fondation/distributions/print.html'
+
+    distributions = Distribution.objects.all()
+
+
+    variable  = {
+            'page_title' : page_title,
+            'distributions' : distributions
         }
 
     return render(
